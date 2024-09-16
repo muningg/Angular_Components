ce57339557183f09769cec4b3845cbad6eaf7d3c
muningg <noesamae@gmail.com>
2024-09-17 07:16:39 +0800

    #4 created counter

---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  count: number = 0;
+  increment() {
+    this.count += 1;
+  }
+}
